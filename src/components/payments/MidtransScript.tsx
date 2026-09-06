14c2b2136bc04e77e42d185a8ed85e81179f5e6f
Ferdy <ferdymu21@gmail.com>
2026-09-06 14:47:16 +0700

    fix: align Midtrans Snap script with payment environment

--- src/components/payments/MidtransScript.tsx
+++ src/components/payments/MidtransScript.tsx
@@ -1,29 +1,21 @@
-"use client";
-
 import Script from "next/script";
 
 export default function MidtransScript() {
+  const isProduction =
+    process.env.MIDTRANS_IS_PRODUCTION === "true";
 
-    return (
+  const snapScriptUrl = isProduction
+    ? "https://app.midtrans.com/snap/snap.js"
+    : "https://app.sandbox.midtrans.com/snap/snap.js";
 
-        <Script
-
-            id="midtrans-script"
-
-            src="https://app.sandbox.midtrans.com/snap/snap.js"
-
-            data-client-key={
-
-                process.env
-
-                    .NEXT_PUBLIC_MIDTRANS_CLIENT_KEY
-
-            }
-
-            strategy="afterInteractive"
-
-        />
-
-    );
-
+  return (
+    <Script
+      id="midtrans-script"
+      src={snapScriptUrl}
+      data-client-key={
+        process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY
+      }
+      strategy="afterInteractive"
+    />
+  );
 }
